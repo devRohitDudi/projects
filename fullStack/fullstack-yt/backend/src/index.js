475059967ae932f67dbf8ff5509acb64f5b00cd7
Rohit dudi <rohitdudijat@yahoo.com>
2025-04-07 19:33:23 +0530

    setup mongodb database for yt-clone

--- fullStack/fullstack-yt/backend/src/index.js
+++ fullStack/fullstack-yt/backend/src/index.js
@@ -1,1 +1,25 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./db/connect.js";
+const app = express();
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//         app.on("error", (error) => {
+//             console.error("Error in Express server:", error);
+//             throw error;
+//         });
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is listening on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//     }
+// })();
